8c8bffc5b861696ccf56d171f3fd2b5b5069832f
yadnyeshkhotre <96002336+yadnyeshkhotre@users.noreply.github.com>
2023-03-09 15:33:13 +0530

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+    // Q1 - Take 2 integer values in two variables x and y and print their product.
+    //  int x=2,y=4,product;
+    //  product=x*y;
+    //  cout<<"product:"<<product<<endl;
+    //return 0;
+
+
+    //  Q2 - Print the ASCII value of character ‘U’.
+    // char U='U';
+    // cout << "ASCII Value of " << U << " is " << int(U);
+    // return 0;
+
+    // Q3 - Write a C++ program to take length and breadth of a rectangle and print its area.
+    // int length,breadth;
+    // cout<<"Enter length and breadth of rectangle: ";
+    // cin>>length>>breadth;
+    // cout<<"Area of reactangle: "<<length*breadth<<endl;
+    // return 0;
+
+    // Q4 - Write a C++ program to calculate the cube of a number.
+    // int number,cube;
+    // cout<<"Enter the number: "; 
+    // cin>>number;
+    // cube=number*number*number;
+    // cout<<"Cube of "<<number<<"is "<<cube<<endl;
+
+    // Q5 - Write a C++ program to find size of basic data types.
+    // cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    // cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    // cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    // cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    // return 0;
+
+    // Q6 - Write a C++ program to swap two numbers with the help of a third variable.
+    // int a,b,c;
+    // a=5,b=10;
+    // cout<<"a :"<<a<<endl<<"b :"<<b<<endl;
+    // cout<<"After swapping: "<<endl;
+    // c=a;
+    // a=b;
+    // b=c;
+    // cout<<"a :"<<a<<endl<<"b :"<<b<<endl;
+
+ }
+
